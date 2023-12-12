a30825f88916127f71bb4bad1d9f9086071c0b9f
TungSoPon <Thanapat.sop@kkumail.com>
2023-12-12 11:52:54 +0700

    testrun1.py

--- firstpy1.py
+++ firstpy1.py
@@ -14,7 +14,7 @@ def parse_input():
         '--XX',
         type=int,
         default=7,
-        help='input for the multiplyby9 function'
+        help='input for XX'
     )
 
     args = parser.parse_args()
@@ -23,12 +23,12 @@ def parse_input():
 def printHello():
     print("Hello world")
 
-def multiplyby9(input_V):
-    print(9 * input_V)
+def multiplyby9(inputV):
+    print(9 * inputV)
 
 if __name__ == "__main__":
     input_V = parse_input()
-    print(f'the input num is {input_V.XX}')
+    print(f'the input XX is {input_V.XX}')
     print("we are in the main function")
     multiplyby9(input_V.num)
     printHello()
