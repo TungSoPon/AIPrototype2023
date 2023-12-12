9603781447641745ff743c5669ae1bd4dea400ec
TungSoPon <Thanapat.sop@kkumail.com>
2023-12-12 11:27:16 +0700

    testrun1.py

--- firstpy1.py
+++ firstpy1.py
@@ -1,29 +1,27 @@
-from unittest.mock import _ArgsKwargs
 import argparse
-from unittest.mock import _ArgsKwargs
 
 def parse_input():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--num',
+        '--XX',
         type=int,
-        required=True,
-        help='input for the multiplyby9 funtion'
+        default=7,
+        help='input for XX'
     )
 
-    args = parser.parse_arge()
-    return _ArgsKwargs
+    args = parser.parse_args()
+    return args
 
 def printHello():
     print("Hello World")
 
 def multiplyby9(inputV):
-    print(9*inputV)
+    print(9 * inputV)
 
-if __name__== "__main__":
-    input_v = parse_input()
-    print(f'the input num is {input_v.num}')
-    print('we are in the main funtion')
-    multiplyby9(20)
+if __name__ == "__main__":
+    input_args = parse_input()
+    print(f'the input XX is {input_args.XX}')
+    print('we are in the main function')
+    multiplyby9(input_args.XX)
     printHello()
