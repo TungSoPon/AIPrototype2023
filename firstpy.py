a7650453296a77465c4cc8cfe133af50d0f8ba36
TungSoPon <Thanapat.sop@kkumail.com>
2023-12-12 10:44:41 +0700

    test run.py

--- firstpy.py
+++ firstpy.py
@@ -4,7 +4,7 @@ def printhello():
 def multiplyby9(inputV):
     print(9*inputV)
 
-if _name__=="__main_": #flow program
+if __name__=="__main_": #flow program
     print("we are in the main function")
     multiplyby9(20)
     printhello()
