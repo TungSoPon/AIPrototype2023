3deb0157c8140af9d6c2f387dc8b783baaa6d6a2
TungSoPon <Thanapat.sop@kkumail.com>
2023-12-12 11:09:26 +0700

    testrun1.py

---
+++ firstpy1.py
@@ -0,0 +1,27 @@
+import argparse
+from unittest.mock import _ArgsKwargs
+
+def parse_input():
+    paese = argparse.ArgumentParser()
+
+    paese.add_argument(
+        '--num',
+        type=int,
+        required=True,
+        help='input for the multiplyby9 funtion'
+    )
+    args = parser.parse_arge()
+    return _ArgsKwargs
+
+def printHello():
+    print("Hello World")
+
+def multiplyby9(inputV):
+    print(9*inputV)
+
+if __name__=="__main__":
+    input_v = parse_input()
+    print(f'the input num is {input_v.num}')
+    print('we are in the main funtion')
+    multiplyby9(20)
+    printHello()
