e61ab482eb21bb94c80ad92458590133da0947ec
TungSoPon <Thanapat.sop@kkumail.com>
2023-12-12 10:26:33 +0700

    test run .py

---
+++ firstpy.py
@@ -0,0 +1,10 @@
+def printhello(): 
+    print("hell word") 
+
+def multiplyby9(inputV):
+    print(9*inputV)
+
+if _name__=="__main_": #flow program
+    print("we are in the main function")
+    multiplyby9(20)
+    printhello()
