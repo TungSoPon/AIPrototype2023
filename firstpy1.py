0644ce1d5d018a6a246abb2fd9db9828a7f8682a
TungSoPon <Thanapat.sop@kkumail.com>
2023-12-12 11:30:43 +0700

    testrun1.py

--- firstpy1.py
+++ firstpy1.py
@@ -4,6 +4,13 @@ def parse_input():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
+        '--num',
+        type=int,
+        default=7,
+        help='input for the multiplyby9 function'
+    )
+
+    parser.add_argument(
         '--XX',
         type=int,
         default=7,
@@ -21,7 +28,10 @@ def multiplyby9(inputV):
 
 if __name__ == "__main__":
     input_args = parse_input()
+    print(f'the input num is {input_args.num}')
     print(f'the input XX is {input_args.XX}')
     print('we are in the main function')
+    multiplyby9(input_args.num)
     multiplyby9(input_args.XX)
     printHello()
+
