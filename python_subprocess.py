461c18bc9597b1cde9062988e9a03052664db327
TungSoPon <Thanapat.sop@kkumail.com>
2023-12-25 15:40:52 +0700

    python_subprocess.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -4,4 +4,4 @@ if __name__ == "__main__" :
     # basic terminal command
     subprocess.run(["ls" , "-ltr"])
     subprocess.run(["rm","-r","/home/thanapat_window/testfolder1"])
-    subprocess.run(["python firstpy1.py --num 48 --XX 58","/home/thanapat_window/codes/AIPrototype2023/firstpy1.py"])
+    subprocess.run(["python", "firstpy1.py", "--num", "48", "--XX", "58","/home/thanapat_window/codes/AIPrototype2023/firstpy1.py"])
