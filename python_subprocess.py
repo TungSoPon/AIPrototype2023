e09e11b1bcd167fbd87721aa181150dc8ea50c59
TungSoPon <Thanapat.sop@kkumail.com>
2023-12-25 16:13:01 +0700

    python_subprocess.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,3 +1,4 @@
+from multiprocessing import process
 import subprocess #สำหรับ terminal command
 
 if __name__ == "__main__" :
@@ -13,3 +14,12 @@ if __name__ == "__main__" :
     print ("third run num=88")
     subprocess.run(["python", "/home/thanapat_window/codes/AIPrototype2023/firstpy1.py", "--num", "88"])
     print ("-----------------------------------")
+
+
+    #use output from other program
+    process_output = subprocess.Popen(["python", "/home/thanapat_window/codes/AIPrototype2023/firstpy1.py", "--num", "88"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out, err = process_output.communicate()
+    print(out.decode('utf-8'))
+    print(len(out.decode('utf-8')))
+
+    #HW เขียน subprocess sum output ทั้งหมด command 3 อันข้างบน (ตัวเลขก่อน Hello world!)
