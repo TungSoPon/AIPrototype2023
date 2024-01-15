903e83a99072d93176b670e1d7307dfe407ccab5
TungSoPon <Thanapat.sop@kkumail.com>
2024-01-15 16:06:21 +0700

    link html

--- firstflask.py
+++ firstflask.py
@@ -4,13 +4,17 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # ถ้าจะใช้คำสั่งนี้ไม่ต้องมี / ก็ได้
 def helloworld():
     return "Hello, World!"
 
-@app.route("/name")
-def hellotung():
+@app.route("/name") # ถ้าจะใช้คำสั่งนี้ต้องมี /name
+def hellotung(): 
     return "Hello, Tung!"
 
-if __name__ == "__main__":
+@app.route("/home2")
+def home2():
+    return render_template("home.html",name='tung')
+
+if __name__ == "__main__": # ต้อง IP VM เราแล้วตามด้วย :5001
     app.run(host='0.0.0.0' ,debug=True,port=5001) #0.0.0.0 คือเข้าได้ในอินเตอร์เน็ท
