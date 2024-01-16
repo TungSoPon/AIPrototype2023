aaeca497eabae60c4ce8dcca10dd64711b17df43
TungSoPon <Thanapat.sop@kkumail.com>
2024-01-16 09:40:56 +0700

    link html

--- firstflask.py
+++ firstflask.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, request, render_template, make_response
 
 import json
@@ -12,9 +13,15 @@ def helloworld():
 def hellotung(): 
     return "Hello, Tung!"
 
-@app.route("/home2")
-def home2():
-    return render_template("home.html",name='tung')
+@app.route("/home", methods=['POST'])
+def homefn():
+    print('we are in home')
+
+    namein = request.form.get('fname')
+    lastname = request.form.get('lname')
+    print(namein)
+    print(lastnamein)
+    return render_template("home.html",name='namein')
 
 if __name__ == "__main__": # ต้อง IP VM เราแล้วตามด้วย :5001
     app.run(host='0.0.0.0' ,debug=True,port=5001) #0.0.0.0 คือเข้าได้ในอินเตอร์เน็ท
