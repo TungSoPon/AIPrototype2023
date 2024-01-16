4419a406d4d8cd9bc8d31e1b81c96183de357712
TungSoPon <Thanapat.sop@kkumail.com>
2024-01-16 11:12:07 +0700

    link html

--- firstflask.py
+++ firstflask.py
@@ -1,6 +1,5 @@
 from crypt import methods
 from flask import Flask, request, render_template, make_response
-
 import sys
 import json
 
@@ -29,6 +28,23 @@ def homefn():
        print(namein, file=sys.stdout)
        print(lastnamein, file=sys.stdout)
        return render_template("home.html",name=namein)
+    
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save('filename')
+        return render_template("home.html",name='uplond completed')
+        
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == "__main__": # ต้อง IP VM เราแล้วตามด้วย :5001
     app.run(host='0.0.0.0' ,debug=True,port=5001) #0.0.0.0 คือเข้าได้ในอินเตอร์เน็ท
